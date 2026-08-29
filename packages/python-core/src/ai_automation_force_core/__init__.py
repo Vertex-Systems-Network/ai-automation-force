9cab82054ab023fbbe2b3ca521605d9061706e15
WPEssential <wordpress.essential@gmail.com>
2026-08-29 20:56:55 +0500

    m03-wp1: expose provider-neutral storage contracts

--- packages/python-core/src/ai_automation_force_core/__init__.py
+++ packages/python-core/src/ai_automation_force_core/__init__.py
@@ -5,4 +5,43 @@ namespace for pre-M01 repository code. New consumers should import this package.
 """
 
 from lullabies_core import *  # noqa: F403
-from lullabies_core import __all__ as __all__
+from lullabies_core import __all__ as _legacy_all
+from lullabies_core.persistence import PostgresStorageObjectRepository, StorageObjectPersistResult
+from lullabies_core.storage import (
+    FilesystemStorageAdapter,
+    StorageAdapter,
+    StorageBackend,
+    StorageBlobStat,
+    StorageConflictError,
+    StorageError,
+    StorageIntegrityError,
+    StorageNotFoundError,
+    StorageObject,
+    StorageObjectId,
+    StorageWriteResult,
+    build_object_key,
+    sha256_bytes,
+    storage_object_from_write,
+    validate_object_key,
+)
+
+__all__ = [
+    *_legacy_all,
+    "FilesystemStorageAdapter",
+    "PostgresStorageObjectRepository",
+    "StorageAdapter",
+    "StorageBackend",
+    "StorageBlobStat",
+    "StorageConflictError",
+    "StorageError",
+    "StorageIntegrityError",
+    "StorageNotFoundError",
+    "StorageObject",
+    "StorageObjectId",
+    "StorageObjectPersistResult",
+    "StorageWriteResult",
+    "build_object_key",
+    "sha256_bytes",
+    "storage_object_from_write",
+    "validate_object_key",
+]
