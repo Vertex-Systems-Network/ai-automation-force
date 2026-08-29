ac141ad2ef33d2401dfdfe57bafdad833d719f02
WPEssential <wordpress.essential@gmail.com>
2026-08-29 19:05:32 +0500

    m02-wp7: export control surface contracts

--- packages/python-core/src/lullabies_core/__init__.py
+++ packages/python-core/src/lullabies_core/__init__.py
@@ -30,6 +30,15 @@ from .common import (
     TimeRange,
 )
 from .content import Content, ContentObjective, ContentVersion
+from .control_surface import (
+    JobCommandConflictError,
+    JobCommandResult,
+    JobCommandVersionConflictError,
+    JobControlSnapshot,
+    JobEventRecord,
+    ProjectJobRecord,
+    control_cursor_key,
+)
 from .entities import Location, Prop, StyleProfile, VoiceProfile, World
 from .job_control import (
     JOB_STATUS_TRANSITIONS,
@@ -69,6 +78,7 @@ from .persistence import (
     PersistResult,
     PostgresApprovalWaitRepository,
     PostgresCircuitBreakerRepository,
+    PostgresControlSurfaceRepository,
     PostgresJobControlRepository,
     PostgresProductionRepository,
     PostgresProviderAsyncRepository,
@@ -189,6 +199,11 @@ __all__ = [
     "GenerationRequest",
     "InvalidJobTransitionError",
     "Job",
+    "JobCommandConflictError",
+    "JobCommandResult",
+    "JobCommandVersionConflictError",
+    "JobControlSnapshot",
+    "JobEventRecord",
     "JobIdempotencyConflictError",
     "JobLeaseConflictError",
     "JobLeaseResult",
@@ -213,6 +228,7 @@ __all__ = [
     "PersistenceShapeError",
     "PostgresApprovalWaitRepository",
     "PostgresCircuitBreakerRepository",
+    "PostgresControlSurfaceRepository",
     "PostgresJobControlRepository",
     "PostgresProductionRepository",
     "PostgresProviderAsyncRepository",
@@ -220,6 +236,7 @@ __all__ = [
     "ProductionLineageBundle",
     "Project",
     "ProjectBundle",
+    "ProjectJobRecord",
     "ProjectStatus",
     "Prop",
     "ProviderAsyncConflictError",
@@ -251,6 +268,7 @@ __all__ = [
     "World",
     "assert_job_transition",
     "assert_provider_async_transition",
+    "control_cursor_key",
     "expected_wait_status",
     "expired_job_status",
     "import_legacy_content_package",
