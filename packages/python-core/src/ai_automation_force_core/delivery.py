dacae1686adac02d5163deb30cea4e0efcc1c1af
WPEssential <wordpress.essential@gmail.com>
2026-09-01 23:45:36 +0500

    M03-WP6: expose delivery contracts

---
+++ packages/python-core/src/ai_automation_force_core/delivery.py
@@ -0,0 +1,27 @@
+from lullabies_core.delivery import (
+    AssetAccessClass,
+    DeliveryAuthorization,
+    DeliveryAuthorizationError,
+    DeliveryAuthorizationKind,
+    DeliveryBindingError,
+    DeliveryMode,
+    DeliverySubject,
+    ShareLinkConstraint,
+    SignedDeliveryGrant,
+    authorize_delivery,
+    bind_delivery_subject,
+)
+
+__all__ = [
+    "AssetAccessClass",
+    "DeliveryAuthorization",
+    "DeliveryAuthorizationError",
+    "DeliveryAuthorizationKind",
+    "DeliveryBindingError",
+    "DeliveryMode",
+    "DeliverySubject",
+    "ShareLinkConstraint",
+    "SignedDeliveryGrant",
+    "authorize_delivery",
+    "bind_delivery_subject",
+]
